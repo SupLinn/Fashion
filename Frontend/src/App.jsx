bc27f8296532acd4ee2461805ab506eacf6e6a87
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-02 22:04:12 +0530

    added searchbox.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,11 +11,14 @@ import  Order  from './pages/Order'
 import  PlaceOrder  from './pages/PlaceOrder'
 import Navbar  from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBox'
+import SearchBox from './components/SearchBox'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar/>
+      <SearchBox/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />

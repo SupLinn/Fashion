0e0a877163be182089f197d595f158fb7777d4a8
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-11 00:22:29 +0530

    added cart functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,10 +13,13 @@ import Navbar  from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBox'
 import SearchBox from './components/SearchBox'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar/>
       <SearchBox/>
       <Routes>
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { ShopContext } from '../context/ShopContext'
 const Navbar = () => {
   const [visible, setVisible] = useState(false)
 
-  const {setShowSearch} = useContext (ShopContext)
+  const {setShowSearch, getCartCount} = useContext (ShopContext)
    return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -55,7 +55,7 @@ const Navbar = () => {
 
             <Link to='/cart' className='relative'>
                <img src={assets.cart_icon} className='w-5 min-w-5' alt=""/>
-               <p className=' absolute right-[-5px] bottom-[-5px] bg-black w-4 text-center leading-4 rounded-full aspect-square text-white text-[8px]'>15</p>
+               <p className=' absolute right-[-5px] bottom-[-5px] bg-black w-4 text-center leading-4 rounded-full aspect-square text-white text-[8px]'>{getCartCount()}</p>
             </Link>
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} alt="" className='w-5 sm:hidden cursor-pointer'/>
        </div>

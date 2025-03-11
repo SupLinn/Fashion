ddbaa6189b3a2aa3f7177c41cfa0ec0f4666f194
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-11 22:04:55 +0530

    add about page

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
- const About = () => {
+const About = () => {
   return (
-    <div></div>
+    <div>
+      <div className='text-2xl text-center border-t mt-16'>
+        <Title text1={'ABOUT '} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full max-w-[450px]'/>
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, officia? Illum quibusdam amet delectus dolor fugit at voluptatibus architecto reiciendis reprehenderit omnis, rerum qui totam. Nobis officiis architecto nulla totam!</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, officia? Illum quibusdam amet delectus dolor fugit at voluptatibus architecto reiciendis reprehenderit omnis, rerum qui totam. Nobis officiis architecto nulla totam!</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla dolor quisquam non unde, eligendi fugiat maxime minus nobis, ut praesentium vitae ullam, illo assumenda qui facilis sapiente aliquam accusamus doloribus!</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY '} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20 gap-2'>
+         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b className='text-md'>Quality Assurance</b>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam mollitia blanditiis ad non atque eaque qui sequi illum culpa totam dolore ex dicta, dolores voluptate deserunt. Nostrum aut natus numquam.</p>
+         </div>
+
+         <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b className='text-md '>Convenience</b>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam mollitia blanditiis ad non atque eaque qui sequi illum culpa totam dolore ex dicta, dolores voluptate deserunt. Nostrum aut natus numquam.</p>
+         </div>
+
+         <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b className='text-md'>Exceptional Customer Service</b>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam mollitia blanditiis ad non atque eaque qui sequi illum culpa totam dolore ex dicta, dolores voluptate deserunt. Nostrum aut natus numquam.</p>
+         </div>
+      </div>
+
+      <NewsletterBox/>
+    </div>
   )
 }
+
 export default About
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useState } from 'react'
+// import { ShopContext } from '../context/ShopContext'
+// import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+
+  const [currState, setCurrState] = useState('Login')
+  // const {token, setToken, navigate, backendUrl} = useContext(ShopContext)
+  // const [name, setName] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [email, setEmail] = useState('')
+  const onSubmitHandler = async (e) => {
+    //console.log("sign up button clicked")
+    e.preventDefault()
+  //   try{
+  //         if(currState==='Sign Up')
+  //         {  //console.log("sign up api now being called")
+  //            const response = await axios.post(backendUrl+'/api/user/register', {name, email, password})
+  //            //console.log(response.data)
+  //            if(response.data.success)
+  //             {setToken(response.data.token)
+  //             localStorage.setItem('token', response.data.token)}
+  //            else
+  //            {
+  //              toast.error(response.data.message)
+  //            } 
+  //         }
+  //         else
+  //         {
+            
+  //                const response = await axios.post(backendUrl+'/api/user/login', {email, password})
+  //                //console.log(response.data)
+  //                if(response.data.success)
+  //                 {setToken(response.data.token)
+  //                 localStorage.setItem('token', response.data.token)}
+  //                else
+  //                {
+  //                  toast.error(response.data.message)
+  //                } 
+  //         }
+  //   }
+  //   catch(e)
+  //   {
+  //      console.log(e)
+  //      toast.error(e.message)
+  //   }
+  }
+
+  // useEffect(()=>
+  // {
+  //   if(token)
+  //     navigate('/')
+  // })
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+      <div className='inline-flex gap-2 mb-2 mt-10 items-center'>
+        <p className='prata-regular text-3xl'>{currState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800'></hr>
+      </div>
+      
+      {currState ==='Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required></input>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required></input>
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required></input>
+      <div className='w-full flex justify-between text-sm mt-[-8px]'>
+      {currState ==='Login' ? <p className='cursor-pointer'>Forgot your password?</p> : ''}  
+      {currState ==='Login' ? <p onClick={()=>setCurrState('Sign Up')} className='cursor-pointer'>Create an account</p> : <p onClick={()=>setCurrState('Login')} className='cursor-pointer'>Already have an account?</p>}
+      </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+    </form>
   )
 }
-export default Login
+
+export default  Login

75b8902882b923259c11271e40f3cd2d17802afe
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-13 21:51:37 +0530

    add product routes

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,27 @@
+
+
+// function for add product
+
+const addProduct = async (req, res) => {
+    
+}
+
+// function for remove product
+
+const removeProduct = async (req, res) => {
+    
+}
+
+// function for list products
+
+const listProducts = async (req, res) => {
+    
+}
+
+// function for single product
+
+const singleProduct = async (req, res) => {
+    
+}
+
+export {addProduct, removeProduct, listProducts, singleProduct}

96f3463c162c627b1a85e6ca33ddaa591ee6c2f1
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-14 00:39:10 +0530

    add productcontroller file

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 
 // App config
+
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
@@ -18,7 +19,7 @@ app.use(cors())
 
 // api endpoints 
 app.use('/api/user', userRouter)
-app.use('api/product', productRouter)
+app.use('/api/product', productRouter)
 
 app.get('/', (req,res)=>{
     res.send("API WORKING...")

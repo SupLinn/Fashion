c6655494b6c0bf74a09ecbb8bffeec714a9c711b
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-16 14:36:15 +0530

    add admin navbar file

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center px-8 py-2'>
+      <img className='' src={assets.logo} alt="" />
+      <button onClick={()=>setToken('')}className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
   )
 }
 

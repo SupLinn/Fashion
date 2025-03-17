cf8d06cad695ae26cc0f8f8813d1bd7584c9c93d
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-17 13:27:44 +0530

    cart functionality added

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = () => {
       setCartData(tempData);
     }
      
-  }, [cartItems])
+  }, [cartItems, products])
   return (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>

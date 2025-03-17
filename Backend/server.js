7134efa8d6f8bd73359c93c830bc343753f15b29
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-17 23:05:25 +0530

    update server file

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
 
 // App config
 
@@ -21,7 +22,8 @@ app.use(cors())
 // api endpoints 
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
-app.use('/api/cart/', cartRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
 
 app.get('/', (req,res)=>{
     res.send("API WORKING...")

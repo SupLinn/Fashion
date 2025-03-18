1b6a1935cee42781e56a2fc28db39d0426198b5b
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2025-03-18 14:50:50 +0530

    update home.jsx file

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
-import Bestseller from '../components/BestSeller'
+import Bestseller from '../components/Bestseller'
 import OurPolicy from '../components/OurPolicy'
 import NewsletterBox from '../components/NewsletterBox'
 
